5c6c1ff5544244b9fe8b548f9dd9cf1b28b1149a
mateusprgm <mateusprgm@gmail.com>
2019-06-23 19:51:56 -0300

    up project

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { Environment, GoogleMap, GoogleMaps, GoogleMapsAnimation, GoogleMapOptions, MyLocation } from '@ionic-native/google-maps';
+// import { LoadingController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,68 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  @ViewChild('map') mapElement:any;
+  private loading:any;
+  map: GoogleMap;
+  
+  constructor(
+    // private platform: Platform,
+    // private loadingCtrl:LoadingController
+    ) {}
+
+  // ngOnInit(){
+  //   this.mapElement = this.mapElement.nativeElement;
+
+  //   this.mapElement.style.width = this.platform.width()+ 'px';
+  //   this.mapElement.style.height = this.platform.height() + 'px';
+
+  //   this.loadMap();
+  // }
+
+  // async loadMap(){
+  //   this.loading = await this.loadingCtrl.create({message:"Por favor, aguarde..."});
+  //   await this.loading.present();
+
+  //   Environment.setEnv({
+  //     'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDAga47tGm5Fh_KZ9I5VRr_a20Ikx-2YPc',
+  //     'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyDAga47tGm5Fh_KZ9I5VRr_a20Ikx-2YPc'
+  //   });
+
+  //   const mapOptions: GoogleMapOptions = {
+  //     zoom:false,
+  //   }
+
+  //   this.map = GoogleMaps.create(this.mapElement, mapOptions);
+
+  //   try {
+  //     this.addOriginMarker();
+  //   } catch (error) {
+      
+  //   }
+  // };
+
+  // async addOriginMarker(){
+  //   try {
+  //     const mylocation: MyLocation = await this.map.getMyLocation();
+
+  //     this.map.moveCamera({
+  //       target: mylocation.latLng,
+  //       zoom:18
+  //     });
+
+  //     this.map.addMarkerSync({
+  //       title:'Origin',
+  //       icon:'#000',
+  //       animation:GoogleMapsAnimation.DROP,
+  //       position:mylocation.latLng
+  //     });
+
+  //   } catch (error) {
+  //     console.error(error);
+  //   } finally {
+  //     this.loading.dismiss();
+  //   }
+      
+  // }
 
 }
