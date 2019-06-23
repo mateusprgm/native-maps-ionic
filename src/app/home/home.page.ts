a4563db0d0d6327ce45084deead0d3e4c45b99e3
mateusprgm <mateusprgm@gmail.com>
2019-06-23 19:54:03 -0300

    mapa funcional

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,6 +1,6 @@
 import { Component, ViewChild } from '@angular/core';
 import { Environment, GoogleMap, GoogleMaps, GoogleMapsAnimation, GoogleMapOptions, MyLocation } from '@ionic-native/google-maps';
-// import { LoadingController, Platform } from '@ionic/angular';
+import { LoadingController, Platform } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
@@ -14,63 +14,63 @@ export class HomePage {
   map: GoogleMap;
   
   constructor(
-    // private platform: Platform,
-    // private loadingCtrl:LoadingController
+    private platform: Platform,
+    private loadingCtrl:LoadingController
     ) {}
 
-  // ngOnInit(){
-  //   this.mapElement = this.mapElement.nativeElement;
+  ngOnInit(){
+    this.mapElement = this.mapElement.nativeElement;
 
-  //   this.mapElement.style.width = this.platform.width()+ 'px';
-  //   this.mapElement.style.height = this.platform.height() + 'px';
+    this.mapElement.style.width = this.platform.width()+ 'px';
+    this.mapElement.style.height = this.platform.height() + 'px';
 
-  //   this.loadMap();
-  // }
+    this.loadMap();
+  }
 
-  // async loadMap(){
-  //   this.loading = await this.loadingCtrl.create({message:"Por favor, aguarde..."});
-  //   await this.loading.present();
+  async loadMap(){
+    this.loading = await this.loadingCtrl.create({message:"Por favor, aguarde..."});
+    await this.loading.present();
 
-  //   Environment.setEnv({
-  //     'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDAga47tGm5Fh_KZ9I5VRr_a20Ikx-2YPc',
-  //     'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyDAga47tGm5Fh_KZ9I5VRr_a20Ikx-2YPc'
-  //   });
+    Environment.setEnv({
+      'API_KEY_FOR_BROWSER_RELEASE': 'AIzaSyDAga47tGm5Fh_KZ9I5VRr_a20Ikx-2YPc',
+      'API_KEY_FOR_BROWSER_DEBUG': 'AIzaSyDAga47tGm5Fh_KZ9I5VRr_a20Ikx-2YPc'
+    });
 
-  //   const mapOptions: GoogleMapOptions = {
-  //     zoom:false,
-  //   }
+    const mapOptions: GoogleMapOptions = {
+      zoom:false,
+    }
 
-  //   this.map = GoogleMaps.create(this.mapElement, mapOptions);
+    this.map = GoogleMaps.create(this.mapElement, mapOptions);
 
-  //   try {
-  //     this.addOriginMarker();
-  //   } catch (error) {
+    try {
+      this.addOriginMarker();
+    } catch (error) {
       
-  //   }
-  // };
+    }
+  };
 
-  // async addOriginMarker(){
-  //   try {
-  //     const mylocation: MyLocation = await this.map.getMyLocation();
+  async addOriginMarker(){
+    try {
+      const mylocation: MyLocation = await this.map.getMyLocation();
 
-  //     this.map.moveCamera({
-  //       target: mylocation.latLng,
-  //       zoom:18
-  //     });
+      this.map.moveCamera({
+        target: mylocation.latLng,
+        zoom:18
+      });
 
-  //     this.map.addMarkerSync({
-  //       title:'Origin',
-  //       icon:'#000',
-  //       animation:GoogleMapsAnimation.DROP,
-  //       position:mylocation.latLng
-  //     });
+      this.map.addMarkerSync({
+        title:'Origin',
+        icon:'#000',
+        animation:GoogleMapsAnimation.DROP,
+        position:mylocation.latLng
+      });
 
-  //   } catch (error) {
-  //     console.error(error);
-  //   } finally {
-  //     this.loading.dismiss();
-  //   }
+    } catch (error) {
+      console.error(error);
+    } finally {
+      this.loading.dismiss();
+    }
       
-  // }
+  }
 
 }
